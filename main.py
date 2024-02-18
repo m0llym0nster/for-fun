6a22c2aba2ae21e48b64add69804a9b998df0c94
m0llym0nster <molly.brinkley@gmail.com>
2024-02-17 23:41:18 -0600

    Update main.py
    run without starting uvicorn - better for containerizing

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
+#!/usr/bin/python
+
 from fastapi import FastAPI
 import pso_sectid
+import uvicorn
 
-"""first time using fast api"""
+"""first time using fast api
+This API will return the Sector ID based on the name you provide it,
+just like PSO ep1 (not blueburst)."""
 
 app = FastAPI()
 @app.get("/sectid/{yourname}")
 async def read_item(yourname):
     sectorid=pso_sectid.pso_sectid(yourname)
     return { yourname : sectorid }
+
+# with this update below, you just need to run the app and have deps installed. 
+# no need to call uvicorn from cli
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, log_level=:"info")
+    #uvicorn.run("main:app", port=8000, host=x.x.x.x, log_level="info")
+    server = uvicorn.Server(config)
+    server.run()
